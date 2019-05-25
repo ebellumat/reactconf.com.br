ebc53f5873872a049fd67be6414be1f1a05d52b9
letgodoy <let.godoy@live.com>
2019-05-25 17:39:02 -0300

    Updates

---
+++ precache-manifest.48f0977fa393fc4e5f4f5c163848fa86.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "34b5293a7ee9e3bb49efcff23c87fd39",
+    "url": "/index.html"
+  },
+  {
+    "revision": "629dfc30bcfcbd31fc2e",
+    "url": "/static/css/2.c2fcad91.chunk.css"
+  },
+  {
+    "revision": "629dfc30bcfcbd31fc2e",
+    "url": "/static/js/2.e5172563.chunk.js"
+  },
+  {
+    "revision": "b17666e18a8573b64a66",
+    "url": "/static/js/main.87ca590a.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "d561eb38531e6296928f8747ced69264",
+    "url": "/static/media/ingresse.d561eb38.png"
+  },
+  {
+    "revision": "19331bfdb82e5cfb273847769224fa36",
+    "url": "/static/media/theater_apcd.19331bfd.jpg"
+  },
+  {
+    "revision": "89cf45f1c0ed134b783b27ae45956f25",
+    "url": "/static/media/vaporwave.89cf45f1.jpg"
+  }
+]);
