da710a775bba7330c6bd380a05fd87a17790e1b3
Gabriel Ferreira <gabriel.ferreira@iclinic.com.br>
2019-09-06 14:35:55 -0300

    feat: add livup

--- src/utils/data/sponsors/juniors.js
+++ src/utils/data/sponsors/juniors.js
@@ -2,28 +2,35 @@ import quanto from '../../../media/images/sponsors/quanto.jpg';
 import ingresse from '../../../media/images/sponsors/ingresse.png';
 import globo from '../../../media/images/sponsors/globo.jpg';
 import pipefy from '../../../media/images/sponsors/pipefy.png';
+import livUp from '../../../media/images/sponsors/livup.png';
 
 export default [
   {
     id: 1,
+    name: 'LivUp',
+    avatar: livUp,
+    link: 'https://www.livup.com.br/',
+  },
+  {
+    id: 2,
     name: 'Ingresse',
     avatar: ingresse,
     link: 'https://www.ingresse.com/',
   },
   {
-    id: 2,
+    id: 3,
     name: 'Globo',
     avatar: globo,
     link: 'https://www.globo.com/',
   },
   {
-    id: 3,
+    id: 4,
     name: 'Pipefy',
     avatar: pipefy,
     link: 'https://www.pipefy.com/',
   },
   {
-    id: 4,
+    id: 5,
     name: 'Quanto',
     avatar: quanto,
     link: 'http://contaquanto.com.br',
