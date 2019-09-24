09993711757b6aeec6c034c5a408d2c114173a2a
letgodoy <let.godoy@live.com>
2019-09-23 22:48:11 -0300

    alterado orden dos patrocinadores

--- src/utils/data/sponsors/juniors.js
+++ src/utils/data/sponsors/juniors.js
@@ -9,33 +9,33 @@ import vagas from '../../../media/images/sponsors/vagas.png';
 export default [
   {
     id: 1,
-    name: 'LivUp',
-    avatar: livUp,
-    link: 'https://www.livup.com.br/',
-  },
-  {
-    id: 2,
-    name: 'Ingresse',
-    avatar: ingresse,
-    link: 'https://www.ingresse.com/',
-  },
-  {
-    id: 3,
-    name: 'Globo',
-    avatar: globo,
-    link: 'https://www.globo.com/',
-  },
-  {
-    id: 4,
     name: 'Pipefy',
     avatar: pipefy,
     link: 'https://www.pipefy.com/',
   },
   {
-    id: 6,
+    id: 2,
+    name: 'LivUp',
+    avatar: livUp,
+    link: 'https://www.livup.com.br/',
+  },
+  {
+    id: 3,
+    name: 'Ingresse',
+    avatar: ingresse,
+    link: 'https://www.ingresse.com/',
+  },
+  {
+    id: 4,
     name: 'Vagas',
     avatar: vagas,
     link: 'https://www.vagas.com.br/',
+  },
+  {
+    id: 6,
+    name: 'Globo',
+    avatar: globo,
+    link: 'https://www.globo.com/',
   },
   {
     id: 7,
